bfe19b8a6b2c90ea6bf71f64f3d533516318fa3a
Cesar Da Silva <113476857+DaSilvaCesar@users.noreply.github.com>
2023-09-30 20:17:19 -0300

    Add files via upload

--- Tarea 3/Ecommerse Funco DC- Finazalido/php/loginDc.php
+++ Tarea 3/Ecommerse Funco DC- Finazalido/php/loginDc.php
@@ -1,13 +1,13 @@
 <?php
-$usuario = $_POST ["usuario"];
-$contrasenia = $_POST ["pass"];
+$usuario = $_POST["usuario"];
+$contrasenia = $_POST["pass"];
 
 $ckusuario= "admin";
-$ckpass = 1234;
+$ckpass = "1234";
 
 if ($usuario==$ckusuario && $contrasenia==$ckpass   ) {
   echo "Correcto";
-  header ("location:index.html");
+  header ("location:../html/login.html");
 } else {
   echo "Incorrecto";
   header ("location:https://support.google.com/webmasters/answer/2445990?hl=es" );
